f0c26118b49965a929defd150bd57ab57f6f67b7
HyelinChoi <imhyell@naver.com>
2024-01-30 17:40:26 +0900

    UPDATED

--- app.py
+++ app.py
@@ -10,6 +10,21 @@ def load_data():
     orders_data = pd.read_csv(orders_file_path)
     return orders_data
 
+@st.cache_data
+def load_product_data():
+    product_file_path = r"./data/random_sample_product.csv"
+    product_data = pd.read_csv(product_file_path)
+    return product_data
+
+def display_product_name_by_id(product_data, product_id):
+    product_info = product_data[product_data['product_id'] == product_id]
+
+    if not product_info.empty:
+        product_name = product_info['product_name'].values[0]
+        st.success(f"Product Name for Product ID {product_id}: {product_name}")
+    else:
+        st.error(f"No product found for Product ID {product_id}.")
+
 
 def display_user_orders(user_id, orders_data):
     user_orders = orders_data[orders_data['user_id'] == user_id]
@@ -19,6 +34,31 @@ def display_user_orders(user_id, orders_data):
 
     else:
         st.write(f"No orders found for User {user_id}.")
+
+def display_days_since_prior_order(orders_data):
+    st.subheader("Distribution of Days Since Prior Order")
+
+    plt.figure(figsize=(10, 6))
+    sns.histplot(orders_data['days_since_prior_order'].dropna(), bins=30, kde=True, color='green')
+    plt.title('Distribution of Days Since Prior Order')
+    plt.xlabel('Days Since Prior Order')
+    plt.ylabel('Frequency')
+
+    st.pyplot(plt.gcf())
+
+def display_order_dow_distribution(orders_data):
+    st.subheader("Order Day of Week Distribution")
+ 
+    order_dow_distribution = orders_data['order_dow'].value_counts().sort_index()
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=order_dow_distribution.index, y=order_dow_distribution.values, color='green')
+    plt.title('Order Day of Week Distribution')
+    plt.xlabel('Day of Week')
+    plt.ylabel('Order Count')
+
+    st.pyplot(plt.gcf())
+
 
 def main():
     orders_data = load_data()
@@ -35,7 +75,10 @@ def main():
     plt.ylabel('Order Frequency')
 
     st.pyplot(plt.gcf())
-    
+
+    display_days_since_prior_order(orders_data)
+    display_order_dow_distribution(orders_data)
+
     st.title("Instacart")
 
     st.write("Please login using your User ID")
@@ -48,6 +91,19 @@ def main():
             display_user_orders(int(user_id), orders_data)
         else:
             st.error("Invalid User ID. Please enter a valid User ID.")
+
+    st.title("Product Lookup Dashboard")
+
+    product_data = load_product_data()
+
+    # Product ID를 입력 받기
+    product_id = st.text_input("Enter Product ID:")
+
+    if st.button("Lookup"):
+        if product_id.isdigit() and int(product_id) in product_data['product_id'].unique():
+            display_product_name_by_id(product_data, int(product_id))
+        else:
+            st.error("Invalid Product ID. Please enter a valid Product ID.")
 
 if __name__ == "__main__":
     main()
